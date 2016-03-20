0ba588f33c370540263dd12e02bc088cea7d0da2
tizz98 <dev.tizz98@gmail.com>
2016-03-20 00:22:54 -0400

    gh-pages

--- android/Grizzhacks/app/src/main/java/org/zumh/android/grizzhacks/Credentials.java
+++
@@ -1,6 +0,0 @@
-package org.zumh.android.grizzhacks;
-
-public class Credentials {
-    public static final String CLIENT_ID = "MoxxRdZQw7by7U8XCDsqWsh-DLCK8zJJzBAgmIps";
-    public static final String CLIENT_SECRET = "ZVhsdfEFNF14Rnflz96DDRYngw44XG49vzUF_GoA";
-}
